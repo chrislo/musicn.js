25aa82c908696f44ab327023ba6bc71d1d6b5384
Chris Lowis / Tom Stuart <chris.lowis@gmail.com>
2014-12-17 15:51:59 +0000

    Play notes using audio buffer
    Render the notes of the score into an AudioBuffer using white noise of
    the correct duration.

--- musicn.js
+++ musicn.js
@@ -31,19 +31,70 @@ AST.Note = function(time, instrument_number, duration, params) {
     this.instrument_number = instrument_number;
     this.duration = duration;
     this.params = params;
+
+    this.add_to_score = function(score) {
+        score.notes.push(new Model.Note(time, duration));
+    }
 };
 
 AST.Terminator = function(time) {
     this.time = time;
+
+    this.add_to_score = function(score) {
+        score.duration = this.time;
+    }
 };
 
 AST.Score = function(data_statements) {
     this.data_statements = data_statements;
+
+    this.to_score = function() {
+        var score = new Model.Score();
+        data_statements.forEach(function(statement) {
+            if(statement.add_to_score) { statement.add_to_score(score) };
+        });
+        return score;
+    };
 };
 
-var score = document.getElementById('score').value
+var Model = {};
+Model.Note = function(time, duration) {
+    this.start = time;
+    this.end = this.start + duration;
+};
+
+Model.Score = function() {
+    this.notes = [];
+
+    this.play = function(context) {
+        var buffer = context.createBuffer(1, this.duration * context.sampleRate, context.sampleRate);
+        var data = buffer.getChannelData(0);
+
+        this.notes.forEach(function(note) {
+            var start = note.start * context.sampleRate;
+            var end = note.end * context.sampleRate;
+
+            for (var i = start; i < end; i++) {
+                data[i] = Math.random() * 2 - 1;
+            }
+        });
+
+        var source = context.createBufferSource();
+        source.buffer = buffer;
+        source.connect(context.destination);
+        source.start();
+    };
+};
+
+var score_source = document.getElementById('score').value
 var grammar = document.getElementById('grammar').value
 
 var parser = PEG.buildParser(grammar);
-var cst = parser.parse(score);
-console.log(cst);
+var ast = parser.parse(score_source);
+console.log(ast);
+
+var score = ast.to_score();
+console.log(score);
+
+var context = new AudioContext();
+score.play(context);
