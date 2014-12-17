c2ea7cae10a8e53c52304e14712e73fac7b52a34
Chris Lowis / Tom Stuart <chris.lowis@gmail.com>
2014-12-17 15:00:09 +0000

    Namespace AST nodes under AST

--- musicn.js
+++ musicn.js
@@ -1,10 +1,12 @@
-var InstrumentDefinition = function(time, number, unit_generators) {
+var AST = {}
+
+AST.InstrumentDefinition = function(time, number, unit_generators) {
     this.time = time;
     this.number = number;
     this.unit_generators = unit_generators;
 };
 
-var Oscillator = function(amplitude, frequency, output, stored_function, initial_value) {
+AST.Oscillator = function(amplitude, frequency, output, stored_function, initial_value) {
     this.amplitude = amplitude;
     this.frequency = frequency;
     this.output = output;
@@ -12,33 +14,32 @@ var Oscillator = function(amplitude, frequency, output, stored_function, initial
     this.initial_value = initial_value;
 };
 
-var Output = function(input, output) {
+AST.Output = function(input, output) {
     this.input = input;
     this.output = output;
 };
 
-var GeneratedFunction = function(time, subroutine_number, function_number, points) {
+AST.GeneratedFunction = function(time, subroutine_number, function_number, points) {
     this.time = time;
     this.subroutine_number = subroutine_number;
     this.function_number = function_number;
     this.points = points;
 };
 
-var Note = function(time, instrument_number, duration, params) {
+AST.Note = function(time, instrument_number, duration, params) {
     this.time = time;
     this.instrument_number = instrument_number;
     this.duration = duration;
     this.params = params;
 };
 
-var Terminator = function(time) {
+AST.Terminator = function(time) {
     this.time = time;
 };
 
-var Score = function(data_statements) {
+AST.Score = function(data_statements) {
     this.data_statements = data_statements;
 };
-
 
 var score = document.getElementById('score').value
 var grammar = document.getElementById('grammar').value
