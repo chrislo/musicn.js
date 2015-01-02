9426af6943971562f426e342afe6f1cc07c4e915
Chris Lowis <chris.lowis@gmail.com>
2015-01-02 12:43:05 +0000

    Move Score parameters on to Score constructor
    A score (in the book I am basing this on) is written with three
    parameters in mind:
    
    - rate
    - blockSize
    - maxAmplitude
    
    The parameters passed to instrument definitions are then in reference to
    these units. For example if the maxAmplitude of a score is 2000, then
    the loudest note that can be played is 2000. Similarly the frequency is
    given in a score in units of (blockSize / rate), so 440Hz when the
    blockSize is 511 and the rate is 20000 is 11.242.
    
    These units are not particularly helpful when doing calculations to
    generate the sample data, so we scale them as soon as possible into
    relative amplitudes (amplitude 1 is maximum) and frequencies (in Hz).

--- musicn.js
+++ musicn.js
@@ -5,11 +5,11 @@ var score_source = document.getElementById('score').value;
 var ast = parser.parse(score_source);
 console.log(ast);
 
-var score = ast.to_score();
+var score = ast.to_score({maxAmplitude: 2047, blockSize: 511, rate: 20000});
 console.log(score);
 
 var context = new AudioContext();
-var data = score.to_data(context.sampleRate, 2047, 511, 20000);
+var data = score.to_data(context.sampleRate);
 var buffer = context.createBuffer(1, data.length, context.sampleRate);
 buffer.copyToChannel(data, 0);
 
--- test.js
+++ test.js
@@ -1,15 +1,32 @@
 var JS = require("jstest")
+var parser = require('./lib/musicn_parser.js')
+var Model = require('./lib/model.js')
 
 JS.Test.describe("compiling a simple score", function() { with(this) {
   var score_source = "TER 8.00 ;\n"
 
   it("generates 8 seconds of audio", function() { with(this) {
-    var parser = require('./lib/musicn_parser.js')
     var ast = parser.parse(score_source);
     var score = ast.to_score();
-    var data = score.to_data(44100, 2047, 511, 20000);
+    var data = score.to_data(44100);
 
     assertEqual(44100 * 8, data.length);
+  }})
+}})
+
+JS.Test.describe("Model.Note", function() { with(this) {
+  it("normalises the amplitude by the scores max amplitude", function() { with(this) {
+    var score = new Model.Score({maxAmplitude: 5});
+    var note = new Model.Note(1, 1, 2, 400, score);
+
+    assertEqual(2/5, note.amplitude);
+  }})
+
+  it("normalises the frequency by the scores max frequency", function() { with(this) {
+    var score = new Model.Score({blockSize: 511, rate: 20000});
+    var note = new Model.Note(1, 1, 2, 0.02555, score);
+
+    assertEqual(1, note.frequency);
   }})
 }})
 
