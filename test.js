9c4caabbbc7f9142e6704e6b01ef3e37f36ef3e3
Chris Lowis <chris.lowis@gmail.com>
2015-01-02 12:02:52 +0000

    A simple integration test
    Make sure that the system generates 8 seconds of data when passed a
    simple score with a TER instruction.
    
    I've had to make the AST object global, as otherwise it isn't in scope
    when the parser is generated on-the-fly under Node. I'm sure there's a
    better way to do this, but I'm not sure what it is.

--- test.js
+++ test.js
@@ -1,9 +1,16 @@
 var JS = require("jstest")
 
-JS.Test.describe("Hello, world!", function() { with(this) {
-    it("runs a test", function() { with(this) {
-        assertEqual( "Hello, world!", ["Hello", "world!"].join(", ") )
-    }})
+JS.Test.describe("compiling a simple score", function() { with(this) {
+  var score_source = "TER 8.00 ;\n"
+
+  it("generates 8 seconds of audio", function() { with(this) {
+    var parser = require('./lib/musicn_parser.js')
+    var ast = parser.parse(score_source);
+    var score = ast.to_score();
+    var data = score.to_data(44100, 2047, 511, 20000);
+
+    assertEqual(44100 * 8, data.length);
+  }})
 }})
 
 JS.Test.autorun()
